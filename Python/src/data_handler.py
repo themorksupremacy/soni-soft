3b8956c2dd4f1d9075ae84371047dca5b49e7f7a
Mark Clifford <mark.clifford229@gmail.com>
2026-03-14 12:37:21 +0000

    Data can now be converted to frequency domain using spectrogram function. Magnitude is now also used as a controller for loudness of cello instrument when using the time domain.

--- Python/src/data_handler.py
+++ Python/src/data_handler.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import socket
 import time
+from scipy.signal import spectrogram
+from scipy.stats import skew, kurtosis
 
 # Code formatting rules:
 # All data structures containing the statistical momements should be written in the same order everytime.
@@ -23,22 +25,39 @@ def file_loader_sim(file_name):
 
 
 def file_loader(file_name):
-    ds = pd.Series()
 
     df = pd.read_csv(file_name)
-    if "B_wave" in df.columns:
-        ds = df
-    else:
-        df.columns = ["B_wave"]
-        ds = df["B_wave"]
 
-    return ds
+    df.columns = df.columns.str.lower()
 
+    if "b_wave" in df.columns:
+        return df
+    
+    #Since B_wave is missing the header can be removed so as to not lose the first row of data.
+    file_name.seek(0)
+    df = pd.read_csv(file_name, header=None)
+
+    cols = ["b_wave"]
+    df.columns = cols
+
+    return df
+    
 def retr_b_wave(data_frame):
+
     try:
-        return data_frame['B_wave']
+        return data_frame['b_wave']
     except Exception as e:
         print('Exception: ', e)
+        return None
+
+def get_mag(dataframe):
+    if 'magnitude' in dataframe.columns:
+        return dataframe['magnitude']
+    else: 
+        return None
+
+def optimal_window():
+    return 1
 
 # Data normalisation
 def normalise_data(dataframe, range_min, range_max):
@@ -71,17 +90,36 @@ def normalise_and_invert(dataframe, range_min, range_max):
 
 
 # Mapping functions
-def map_all_stats(data_series, window_size):
+def map_all_stats_tdom(data_series, window_size, magnitude):
     x = data_series.rolling(window=window_size)
 
+    if magnitude is None:
+        magnitude = [0.5] * len(data_series)
+    else:
+        magnitude = [np.nan]*(window_size-1) + normalise_data(magnitude[window_size-1:], range_min=0.3, range_max=1)
+    
     return pd.DataFrame(
         {
             "mean": normalise_data(x.mean(), range_min=50, range_max=105),
             "skew": normalise_data(x.skew(), range_min=-1, range_max=1),
             "std": normalise_data(x.std(), range_min=50, range_max=127),
             "kurtosis": normalise_and_invert(x.kurt(), range_min=50, range_max=300),
+            "magnitude": magnitude,
         }
     )
+
+def map_all_stats_fdom(data_frame):
+    magnitude = [0.5] * len(data_frame["mean"])
+    
+    return pd.DataFrame(
+    {
+        "mean": normalise_data(data_frame["mean"], range_min=50, range_max=105),
+        "skew": normalise_data(data_frame["skew"], range_min=-1, range_max=1),
+        "std": normalise_data(data_frame["std"], range_min=50, range_max=127),
+        "kurtosis": normalise_and_invert(data_frame["kurtosis"], range_min=50, range_max=300),
+        "magnitude": magnitude,
+    })
+
 
 # Diagram Generation
 
@@ -92,21 +130,45 @@ def gen_freq_dom_comparison():
     return 0
 
 # Fast Fourier Transforms (FFTs)
+def compute_stfft(dataseries, nperseg_val, noverlap_val, sampling_freq):
+    f_spec_original, t_spec_original, Sxx_original = spectrogram(
+        dataseries, fs=sampling_freq, nperseg=nperseg_val, noverlap=noverlap_val
+        )
+    
+    mean_vals = np.mean(Sxx_original, axis=1)
+    skew_vals = skew(Sxx_original, axis=1)
+    std_vals = np.std(Sxx_original, axis=1)
+    kurt_vals = kurtosis(Sxx_original, axis=1)
+    
+    Sxx_stats = pd.DataFrame(
+        {
+            "mean": mean_vals,
+            "skew": skew_vals,
+            "std": std_vals,
+            "kurtosis": kurt_vals,
+        }
+    )
 
+    return Sxx_stats
 
 # UDP Transfer
 def send_over_UDP(dataframe, host="127.0.0.1", port=8888, delay=0.1, socketio=None):
+    
+    import flask_app
+    
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        for mean, skew, std, kurtosis in zip(
+        for mean, skew, std, kurtosis, magnitude in zip(
             dataframe["mean"].to_numpy(),
             dataframe["skew"].to_numpy(),
             dataframe["std"].to_numpy(),
             dataframe["kurtosis"].to_numpy(),
+            dataframe["magnitude"].to_numpy(),
         ):
+            
             if np.isnan(mean):
                 continue
 
-            msg = f"{mean} {skew} {std} {kurtosis};\n"
+            msg = f"{mean} {skew} {std} {kurtosis} {magnitude};\n"
             s.sendto(msg.encode("utf-8"), (host, port))
 
             # Emit to frontend
@@ -118,7 +180,8 @@ def send_over_UDP(dataframe, host="127.0.0.1", port=8888, delay=0.1, socketio=No
                         "skew": round(skew, 2),
                         "std": round(std, 2),
                         "kurtosis": round(kurtosis, 2),
+                        "magnitude": round(magnitude, 2),
                     },
                 )
 
-            time.sleep(delay)
+            time.sleep(flask_app.current_delay)
--- Python/src/flask_app.py
+++ Python/src/flask_app.py
@@ -6,12 +6,17 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
+current_delay = 0.99
 
+@socketio.on("update_delay")
+def update_delay(data):
+    global current_delay
+    current_delay = float(data["delay"])
+    print("New delay:", current_delay)
 
 @app.route("/")
 def index():
     return render_template("form.html")
-
 
 @app.route("/display", methods=["POST"])
 def display():
@@ -22,17 +27,21 @@ def display():
     port = int(request.form["port"])
     delay = float(request.form["delay"])
 
-    print(file)
-
     dataset = data_handler.file_loader(file)
-    stats = data_handler.map_all_stats(data_handler.retr_b_wave(dataset), 
-                                       window_size)
+
+    # df = data_handler.retr_b_wave(dataset)
+    # dff =data_handler.compute_stfft(df, 1024, 512, 35000)
+
+    stats = data_handler.map_all_stats_tdom(data_handler.retr_b_wave(dataset), 
+                                       window_size, magnitude=data_handler.get_mag(dataset))
+    #stats = data_handler.map_all_stats_fdom(dff)
 
     socketio.start_background_task(
         data_handler.send_over_UDP, stats, "127.0.0.1", port, delay, socketio
     )
 
     return render_template("display.html")
+
 
 
 if __name__ == "__main__":
