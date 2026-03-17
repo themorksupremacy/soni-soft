51569a4622a409698ed2f22b01e521f64f5c1a49
themorksupremacy <mark.clifford229@gmail.com>
2026-03-17 09:06:29 +0000

    Added correlation heatmaps. Fixed bug where statistical moments were being incorrectly calculated for the frequency domain.

--- Python/src/data_handler.py
+++ Python/src/data_handler.py
@@ -6,6 +6,7 @@ import time
 from scipy.signal import spectrogram
 from scipy.stats import skew, kurtosis
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Code formatting rules:
 # All data structures containing the statistical momements should be written in the same order everytime.
@@ -33,8 +34,8 @@ def file_loader(file_name):
 
     if "b_wave" in df.columns:
         return df
-    
-    #Since B_wave is missing the header can be removed so as to not lose the first row of data.
+
+    # Since B_wave is missing the header can be removed so as to not lose the first row of data.
     file_name.seek(0)
     df = pd.read_csv(file_name, header=None)
 
@@ -42,23 +43,27 @@ def file_loader(file_name):
     df.columns = cols
 
     return df
-    
+
+
 def retr_b_wave(data_frame):
 
     try:
-        return data_frame['b_wave']
+        return data_frame["b_wave"]
     except Exception as e:
-        print('Exception: ', e)
+        print("Exception: ", e)
         return None
 
+
 def get_mag(dataframe):
-    if 'magnitude' in dataframe.columns:
-        return dataframe['magnitude']
-    else: 
+    if "magnitude" in dataframe.columns:
+        return dataframe["magnitude"]
+    else:
         return None
+
 
 def optimal_window():
     return 1
+
 
 # Data normalisation
 def normalise_data(dataframe, range_min, range_max):
@@ -97,8 +102,10 @@ def map_all_stats_tdom(data_series, window_size, magnitude):
     if magnitude is None:
         magnitude = [0.5] * len(data_series)
     else:
-        magnitude = [np.nan]*(window_size-1) + normalise_data(magnitude[window_size-1:], range_min=0.3, range_max=1)
-    
+        magnitude = [np.nan] * (window_size - 1) + normalise_data(
+            magnitude[window_size - 1 :], range_min=0.3, range_max=1
+        )
+
     return pd.DataFrame(
         {
             "mean": normalise_data(x.mean(), range_min=50, range_max=105),
@@ -109,46 +116,123 @@ def map_all_stats_tdom(data_series, window_size, magnitude):
         }
     )
 
+
 def map_all_stats_fdom(data_frame):
     magnitude = [0.5] * len(data_frame["mean"])
-    
+
     return pd.DataFrame(
-    {
-        "mean": normalise_data(data_frame["mean"], range_min=50, range_max=105),
-        "skew": normalise_data(data_frame["skew"], range_min=-1, range_max=1),
-        "std": normalise_data(data_frame["std"], range_min=50, range_max=127),
-        "kurtosis": normalise_data(data_frame["kurtosis"], range_min=50, range_max=127),
-        "magnitude": magnitude,
-    })
+        {
+            "mean": normalise_data(data_frame["mean"], range_min=50, range_max=105),
+            "skew": normalise_data(data_frame["skew"], range_min=-1, range_max=1),
+            "std": normalise_data(data_frame["std"], range_min=50, range_max=127),
+            "kurtosis": normalise_data(
+                data_frame["kurtosis"], range_min=50, range_max=127
+            ),
+            "magnitude": magnitude,
+        }
+    )
 
 
 # Diagram Generation
 
+
 def gen_Spectogram():
     return 0
 
+
 def gen_freq_dom_comparison():
     return 0
+
+
+def specific_freq_band(dataseries, nperseg_val, noverlap_val, sampling_freq):
+
+    f_spec_original, t_spec_original, Sxx_original = spectrogram(
+        dataseries, fs=sampling_freq, nperseg=nperseg_val, noverlap=noverlap_val
+    )
+
+    f_low = 4000
+    f_high = 5000
+
+    idx = np.where((f_spec_original >= f_low) & (f_spec_original <= f_high))[0]
+
+    band = Sxx_original[idx, :]
+
+    Sxx_stats = pd.DataFrame(
+        {
+            "mean": np.mean(band, axis=0),
+            "skew": skew(band, axis=0),
+            "std": np.std(band, axis=0),
+            "kurtosis": kurtosis(band, axis=0),
+        }
+    )
+
+    # Spectrogram
+    plt.figure(figsize=(12, 6))
+    plt.pcolormesh(
+        t_spec_original,
+        f_spec_original,
+        10 * np.log10(Sxx_original),
+        shading="gouraud",
+    )
+    plt.ylabel("Frequency [Hz]")
+    plt.xlabel("Time [sec]")
+    plt.ylim(0, 5000)
+    plt.colorbar(label="Intensity [dB]")
+    plt.savefig(r"Python\src\Spectrogram.png")
+
+    # Correlation heatmap
+    plt.figure()
+    corr_mtx = Sxx_stats.corr()
+    sns.heatmap(corr_mtx, cmap="YlGnBu", annot=True)
+    plt.title("Correlation heatmap")
+    plt.savefig(r"Python\src\heatmap.png")
+
+    return Sxx_stats
+
 
 # Fast Fourier Transforms (FFTs)
 def compute_stfft(dataseries, nperseg_val, noverlap_val, sampling_freq):
     f_spec_original, t_spec_original, Sxx_original = spectrogram(
         dataseries, fs=sampling_freq, nperseg=nperseg_val, noverlap=noverlap_val
-        )
-    
-    mean_vals = np.mean(Sxx_original, axis=1)
-    skew_vals = skew(Sxx_original, axis=1)
-    std_vals = np.std(Sxx_original, axis=1)
-    kurt_vals = kurtosis(Sxx_original, axis=1)
-    
-    Sxx_stats = pd.DataFrame(
-        {
-            "mean": mean_vals,
-            "skew": skew_vals,
-            "std": std_vals,
-            "kurtosis": kurt_vals,
-        }
     )
+
+    # mean_vals = np.mean(Sxx_original, axis=0)
+    # skew_vals = skew(Sxx_original, axis=0)
+    # std_vals = np.std(Sxx_original, axis=0)
+    # kurt_vals = kurtosis(Sxx_original, axis=0)
+
+    # Sxx_stats = pd.DataFrame(
+    #     {
+    #         "mean": mean_vals,
+    #         "skew": skew_vals,
+    #         "std": std_vals,
+    #         "kurtosis": kurt_vals,
+    #     }
+    # )
+
+    Sxx_df = pd.DataFrame(Sxx_original)
+
+    stats_list = []
+
+    for col in Sxx_df.columns:
+        colSeries = Sxx_df[col]
+
+        stats_list.append(
+            {
+                "mean": colSeries.mean(),
+                "skew": colSeries.skew(),
+                "std": colSeries.std(),
+                "kurtosis": colSeries.kurtosis(),
+            }
+        )
+
+    Sxx_stats = pd.DataFrame(stats_list)
+
+    # Plot correlation heatmap
+    corr_mtx = Sxx_stats.corr(numeric_only=True)
+    sns.heatmap(corr_mtx, cmap="YlGnBu", annot=True)
+    plt.savefig(r"Python\src\heatmap.png")
+    plt.title("Correlation heatmap")
 
     # Plot the spectrogram
     plt.figure(figsize=(12, 6))
@@ -168,9 +252,17 @@ def compute_stfft(dataseries, nperseg_val, noverlap_val, sampling_freq):
 
     return Sxx_stats
 
+
 # UDP Transfer
-def send_over_UDP(dataframe, host="127.0.0.1", port=8888, get_delay=None, socketio=None, stop_event=None):
-    
+def send_over_UDP(
+    dataframe,
+    host="127.0.0.1",
+    port=8888,
+    get_delay=None,
+    socketio=None,
+    stop_event=None,
+):
+
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         for mean, skew, std, kurtosis, magnitude in zip(
             dataframe["mean"].to_numpy(),
@@ -179,11 +271,11 @@ def send_over_UDP(dataframe, host="127.0.0.1", port=8888, get_delay=None, socket
             dataframe["kurtosis"].to_numpy(),
             dataframe["magnitude"].to_numpy(),
         ):
-            
+
             if stop_event.is_set():
                 print("Sonification stopped")
                 break
-            
+
             if np.isnan(mean):
                 continue
 
--- Python/src/flask_app.py
+++ Python/src/flask_app.py
@@ -4,6 +4,7 @@ import threading
 from flask_socketio import SocketIO
 import os
 import matplotlib
+
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -15,28 +16,36 @@ current_delay = 0.99
 stop_event = threading.Event()
 b_wave = pd.Series()
 sampling_freq = 35000
-base_name = ''
+base_name = ""
+
 
 @app.route("/download_spectrogram")
 def download():
-    return send_file("Spectrogram.png", as_attachment=True)
-    
+    return send_file(
+        "Spectrogram.png", as_attachment=True, download_name=f"{base_name}.png"
+    )
+
+
 @socketio.on("update_delay")
 def update_delay(data):
     global current_delay
     current_delay = float(data["delay"])
     print("New delay:", current_delay)
 
+
 def get_current_delay():
     return current_delay
+
 
 @socketio.on("stop_sonification")
 def stop_sonification():
     stop_event.set()
 
+
 @app.route("/")
 def index():
     return render_template("form.html")
+
 
 @app.route("/display", methods=["POST"])
 def display():
@@ -58,15 +67,24 @@ def display():
     b_wave = data_handler.retr_b_wave(dataset)
 
     if domain == "frequency":
-        dff = data_handler.compute_stfft(b_wave, 1024, 512, 35000)
+        # dff = data_handler.compute_stfft(b_wave, 1024, 512, 35000)
+        dff = data_handler.specific_freq_band(b_wave, 1024, 512, 35000)
         stats = data_handler.map_all_stats_fdom(dff)
     else:
-        stats = data_handler.map_all_stats_tdom(data_handler.retr_b_wave(dataset), 
-                                    window_size, magnitude=data_handler.get_mag(dataset))
-    
+        stats = data_handler.map_all_stats_tdom(
+            data_handler.retr_b_wave(dataset),
+            window_size,
+            magnitude=data_handler.get_mag(dataset),
+        )
 
     socketio.start_background_task(
-        data_handler.send_over_UDP, stats, "127.0.0.1", port, get_current_delay, socketio, stop_event
+        data_handler.send_over_UDP,
+        stats,
+        "127.0.0.1",
+        port,
+        get_current_delay,
+        socketio,
+        stop_event,
     )
 
     return render_template("display.html", delay=current_delay)
